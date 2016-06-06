90311ccbd6d6f9cadeabea44b650e317c865dcfb
Daniel Hoerauf <dah4pe@virginia.edu>
2016-06-06 10:00:57 -0400

    Additional Unit Tests

--- tests/datadog/test_datadog_controller.py
+++ tests/datadog/test_datadog_controller.py
@@ -15,23 +15,43 @@ class DatadogMonitoringTest(TestCase):
 		names = [monitor.get('name') for monitor in controller.monitor_data]
 		self.assertEquals(['[single-test] MongoDB Single Test'], names)
 
+		controller = self._datadog_controller('multi_monitor')
+		[controller._resolve_monitor(mon) for mon in controller.monitor_data]
+		names = [monitor.get('name') for monitor in controller.monitor_data]
+		self.assertEquals(['Datadog Multi Test', 'Datadog Multi Test'], names)
+
 	def test_monitor_tags(self):
 		controller = self._datadog_controller('single_monitor')
 		[controller._resolve_monitor(mon) for mon in controller.monitor_data]
 		tagses = [monitor.get('tags') for monitor in controller.monitor_data]
 		self.assertEquals([['clustername:single-test', 'monitor:mongodb-single-test']], tagses)
 	
+		controller = self._datadog_controller('multi_monitor')
+		[controller._resolve_monitor(mon) for mon in controller.monitor_data]
+		tagses = [monitor.get('tags') for monitor in controller.monitor_data]
+		self.assertEquals([['clustername:multi-test', 'monitor:datadog-multi-1'], ['clustername:multi-test', 'monitor:datadog-multi-2']], tagses)
+
 	def test_monitor_query(self):
 		controller = self._datadog_controller('single_monitor')
 		[controller._resolve_monitor(mon) for mon in controller.monitor_data]
 		queries = [monitor.get('query') for monitor in controller.monitor_data]
 		self.assertEquals(['max(last_1h):avg:mongodb.replset.replicationlag{clustername:single-test} by {nodeid} < -10'], queries)
 
+		controller = self._datadog_controller('multi_monitor')
+		[controller._resolve_monitor(mon) for mon in controller.monitor_data]
+		queries = [monitor.get('query') for monitor in controller.monitor_data]
+		self.assertEquals(['max(last_1h):avg:test{clustername:multi-test}' for i in range(2)], queries)
+
 	def test_monitor_message(self):
 		controller = self._datadog_controller('single_monitor')
 		[controller._resolve_monitor(mon) for mon in controller.monitor_data]
 		messages = [monitor.get('message') for monitor in controller.monitor_data]
 		self.assertEquals(['Test Single @slack-arc-platform-ops'], messages)
+
+		controller = self._datadog_controller('multi_monitor')
+		[controller._resolve_monitor(mon) for mon in controller.monitor_data]
+		messages = [monitor.get('message') for monitor in controller.monitor_data]
+		self.assertEquals(['Test Multi 1 @slack-notify-one @slack-notify-two', 'Test Multi 2 @slack-notify-one @slack-notify-two'], messages)
 
 	def _datadog_controller(self, config_dir):
 		base_dir = join(TEST_ROOT, config_dir)
