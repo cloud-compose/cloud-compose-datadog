2cc6464e637c07fe23a3d7a897dfd98e48309678
Patrick Cullen <patrick.cullen@washpost.com>
2016-06-13 12:15:40 -0400

    added friendly error message instead of stack trace

--- cloudcompose/datadog/commands/cli.py
+++ cloudcompose/datadog/commands/cli.py
@@ -13,11 +13,14 @@ def monitors(mode):
     """
     Create or delete Datadog monitors defined in cloudcompose.yml
     """
-    cloud_config = CloudConfig()
-    datadog_controller = DatadogController(cloud_config)
-    if mode.lower() == 'up':
-        datadog_controller.up()
-    elif mode.lower() == 'down':
-        datadog_controller.down()
-    else:
-        raise CloudComposeException('{} not a valid argument'.format(mode))
+    try:
+        cloud_config = CloudConfig()
+        datadog_controller = DatadogController(cloud_config)
+        if mode.lower() == 'up':
+            datadog_controller.up()
+        elif mode.lower() == 'down':
+            datadog_controller.down()
+        else:
+            raise CloudComposeException('{} not a valid argument'.format(mode))
+    except CloudComposeException as ex:
+        print ex.message
