2bd975f7b7feda770c2db777da509772cb45deaa
Jason Bartz <jason.bartz@washpost.com>
2017-04-17 17:59:42 -0400

    missed version update on last PR

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import warnings
 
 setup(
     name='cloud-compose-datadog',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
